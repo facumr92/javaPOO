35237e304ef7924a276657a690a0350f3853d3c5
Facundo Rubil <facundorubil@MacBook-Air-de-Facundo.local>
2025-04-21 15:46:29 -0300

    subida1

---
+++ JAVA/celular.java
@@ -0,0 +1,40 @@
+
+// Clase celular
+class celular {
+    // Atributos (características)
+
+    String marca;
+    String modelo;
+    int anio;
+
+    // Constructor (para crear un celular con  todos sus datos)
+    public celular(String marca, String modelo, int anio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+    }
+
+    // Método llamar
+    public void llamar() {
+        System.out.println(" llamando por celular desde mi..."+ modelo);
+    }
+
+    // Método para mostrar información del celular
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca + ", modelo: " + modelo + "año:" + anio);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // Crear un objeto de la clase Celular 
+
+        celular micCelular = new celular("Apple", "Iphone", 2020);
+
+        // Llamar a métodos del objeto
+        
+        micCelular.mostrarInfo();
+        micCelular.llamar();
+}
+}
