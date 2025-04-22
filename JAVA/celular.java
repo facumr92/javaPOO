df3dbf7ecf0914424ae0fd09b30054a51fbac99f
Facundo Rubil <facundorubil@MacBook-Air-de-Facundo.local>
2025-04-22 15:12:48 -0300

    añado artefactos

--- JAVA/celular.java
+++ JAVA/celular.java
@@ -1,4 +1,5 @@
 
+import java.util.*;
 // Clase celular
 class celular {
     // Atributos (características)
@@ -6,29 +7,31 @@ class celular {
     String marca;
     String modelo;
     int anio;
-    int bateria;
+    int camara;
 
     // Constructor (para crear un celular con  todos sus datos)
-    public celular(String marca, String modelo, int anio, int bateria) {
+    public celular(String marca, String modelo, int anio, int camara) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
-        this.bateria= bateria;
+        this.camara= camara;
     }
 
     // Método llamar
     public void llamar() {
 
-        if(bateria<=5){
+     /*    if(bateria<=5){
 
             System.out.println("No tienes suficiente bateria!");
-        }else
+        }else*/ 
         System.out.println(" llamando por celular desde mi..."+ modelo);
     }
 
+
+
     // Método para mostrar información del celular
     public void mostrarInfo() {
-        System.out.println("Marca: " + marca + ", modelo: " + modelo + "año:" + anio);
+        System.out.println("Marca: " + marca + ", modelo: " + modelo + "año: " + anio+ "tiene de camara: "+ camara+"mpx");
     }
 
     public void enviarMsj(){
@@ -37,11 +40,40 @@ class celular {
 
 
 
+    public static celular crearCelu(){
+
+
+        Scanner dato= new Scanner(System.in);
+
+
+        System.out.println("Que marca es su celular?");
+        String marca= dato.nextLine();
+
+        System.out.println("Modelo?");
+        String modelo= dato.nextLine();
+
+        System.out.println("Año?");
+        int anio= dato.nextInt();
+
+        System.out.println("Cuanto de camara?(mpx):");
+        int camara= dato.nextInt();
+
+        return new celular(marca, modelo, anio, camara);
+
+      
+    }
+
     public static void main(String[] args) {
 
         // Crear un objeto de la clase Celular 
 
-        celular micCelular = new celular("Apple", "Iphone X", 2020, 3);
+
+
+
+        // celular micCelular = new celular("Apple", "Iphone X", 2020, 3);
+
+
+        celular  micCelular =  celular.crearCelu();
 
         // Llamar a métodos del objeto
         
